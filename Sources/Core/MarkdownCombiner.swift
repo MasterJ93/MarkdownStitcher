5a322f2121a9b239d9818a436c9c26167a01150c
Christopher Jr Riley <chrisjr@cjrriley.com>
2024-12-03 01:31:51 -0500

    Add delimiter for separating files

--- Sources/Core/MarkdownCombiner.swift
+++ Sources/Core/MarkdownCombiner.swift
@@ -11,14 +11,27 @@ public struct MarkdownCombiner {
     public static func combine(filePaths: [String]) -> String {
         var combinedContent = ""
 
-        for file in filePaths {
+        for (_, file) in filePaths.enumerated() {
             if let content = try? String(contentsOfFile: file) {
-                combinedContent.append("\n\(content)")
+                // Add a delimiter before appending content
+                let delimiter = createDelimiter(for: file)
+                combinedContent.append(delimiter)
+                combinedContent.append("\n\(content)\n")
             } else {
                 print("Failed to read file: \(file)")
             }
         }
 
         return combinedContent
+    }
+
+    private static func createDelimiter(for filePath: String) -> String {
+        let fileName = (filePath as NSString).lastPathComponent
+        let delimiter = """
+        \n\n
+        -------- START OF FILE: \(fileName) --------
+        \n\n
+        """
+        return delimiter
     }
 }
